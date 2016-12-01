ca705411163d9901d32fd15282f0d65feb47cc80
Warren Hansen <15slim@warrens-macbook-pro.local>
2016-11-30 16:59:48 -0800

    task: set up picker

---
+++ Camera Order List/Equipment Classes.swift
@@ -0,0 +1,69 @@
+//
+//  Equipment Classes.swift
+//  Camera Order List
+//
+//  Created by Warren Hansen on 11/30/16.
+//  Copyright © 2016 Warren Hansen. All rights reserved.
+//
+
+import Foundation
+// Set up objects for camera order from what I learned at Udacity
+//  create un mutable types of equipment objects
+enum Catagory {
+    case camera, primes, macros, probeLens, zoomLens, aks, finder, filters, support
+    
+    static let allValues = ["Camera", "Primes", "Macros", "ProbecLens", "Zoom Lens", "AKS", "Finder", "Filters", "Support"]
+}
+
+enum Maker {
+    case arri, red, phantom, panavision, sony
+}
+
+enum Model {
+    case alexa, alexaXT, red, phantom, dragon
+}
+
+//  create a mutable equipment order object
+class Equipment {
+    var quantity: Int       //  1
+    var catagory: Catagory          //  Camera       Prime, Macro, Probe, Zoom
+    var maker: Maker        //  Arri         Zeiss
+    var model: Model        //  Alexa XT     Master Prime
+    var kit: Array<String> = Array()   //  Nil          12mm, 14mm, 16mm ect
+    
+    init(quantity: Int, catagory: Catagory, maker: Maker, model: Model, kit: Array<String>) {
+        self.quantity = quantity
+        self.catagory = catagory
+        self.maker = maker
+        self.model = model
+        self.kit = kit
+    }
+}
+
+class User {
+    var name: String
+    var production: String
+    var company: String
+    var city: String
+    var date: String
+    var weather: String
+    //var logo: NSData
+    init(name: String, production: String, company: String, city: String, date: String, weather: String) {
+        self.name = name
+        self.production = production
+        self.company = company
+        self.city = city
+        self.date = date
+        self.weather = weather
+    }
+}
+
+class Event {
+    var user: User
+    var equipment: Equipment
+    
+    init(user: User, equipment: Equipment){
+        self.user = user
+        self.equipment = equipment
+    }
+}
--- Camera Order List/ViewController.swift
+++ Camera Order List/ViewController.swift
@@ -8,18 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    @IBOutlet weak var myPicker: UIPickerView!
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
+    // Input data into the Array:
+    var pickerData = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.myPicker.dataSource = self
+        self.myPicker.delegate = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // Mark: - Picker delegates and controls
+    // The number of columns of data
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
     }
-
-
+    // The number of rows of data
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    // The data to return for the row and component (column) that's being passed in
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
+    // populate the textbox
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        myLabel.text = pickerData[row]
+    }
+    
 }
 
